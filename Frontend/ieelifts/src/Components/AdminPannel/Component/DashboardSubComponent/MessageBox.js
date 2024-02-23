c431ec7b1278b1a72b8f87aef13cdf8a9fdc1d42
Preet <psqrco@gmail.com>
2024-02-23 10:47:29 +0530

    final message box and socket implemention

--- Frontend/ieelifts/src/Components/AdminPannel/Component/DashboardSubComponent/MessageBox.js
+++ Frontend/ieelifts/src/Components/AdminPannel/Component/DashboardSubComponent/MessageBox.js
@@ -1,4 +1,5 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useState, useEffect,
+  useLayoutEffect } from "react";
 import { RxCross2 } from "react-icons/rx";
 import { MdSend } from "react-icons/md";
 import { MdAddCall } from "react-icons/md";
@@ -7,37 +8,45 @@ import { MdAddCall } from "react-icons/md";
 import { MdOutlineMic } from "react-icons/md";
 import { MdOutlineAttachFile } from "react-icons/md";
 
-import { useDispatch , useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { createChatActions } from "../../../../ReduxSetup/Actions/ChatActions"
 import { sendChatMessageAction } from "../../../../ReduxSetup/Actions/ChatActions"
 import { getSenderMessagesAction } from "../../../../ReduxSetup/Actions/ChatActions"
 
 import { io } from 'socket.io-client';
 
-const MessageBox = ({ onClose,EnggId }) => {
+const MessageBox = ({ onClose, EnggId }) => {
   const dispatch = useDispatch();
 
-  // console.log("clicked EnggId",EnggId);
+  const fileInputField = useRef(null);
+  const textareaRef = useRef();
+  const messageBodyRef = useRef(null);
+
 
   const [messageData, setMessageData] = useState();
-
   const [socketConnected, setSocketConnected] = useState(false);
- 
-  // console.log(messageData)
-
-
   const [file, setFile] = useState(false);
-  const fileInputField = useRef(null);
+  const [textareaHeight, setTextareaHeight] = useState();
+  const [swapIcon, setSwapIcon] = useState(true);
+
+  const scroll = () => {
+    if (messageBodyRef.current) {
+      messageBodyRef.current.scrollTop = messageBodyRef.current.scrollHeight;
+    }
+  };
+
+
+
+
+
   const handleFileChange = (e) => {
     setFile(e.target.files[0].name);
   };
 
-  const textareaRef = useRef(null);
-  const [textareaHeight, setTextareaHeight] = useState();
-  const [swapIcon, setSwapIcon] = useState(true);
 
   useEffect(() => {
     setHeight(textareaRef.current);
+
   }, []);
 
 
@@ -60,9 +69,9 @@ const MessageBox = ({ onClose,EnggId }) => {
 
 
   const chatCreated = useSelector((state) => {
-    if(state.ChatRootReducer && state.ChatRootReducer.createChatReducer && state.ChatRootReducer.createChatReducer.createChat){
+    if (state.ChatRootReducer && state.ChatRootReducer.createChatReducer && state.ChatRootReducer.createChatReducer.createChat) {
       return state.ChatRootReducer.createChatReducer.createChat.FullChat
-    }else{
+    } else {
       return null
     }
   });
@@ -70,23 +79,37 @@ const MessageBox = ({ onClose,EnggId }) => {
 
 
   const getMessages = useSelector((state) => {
-    if(state.ChatRootReducer && state.ChatRootReducer.getSenderMessagesReducer && state.ChatRootReducer.getSenderMessagesReducer.message){
+
+scroll();
+    if (state.ChatRootReducer && state.ChatRootReducer.getSenderMessagesReducer && state.ChatRootReducer.getSenderMessagesReducer.message) {
+   
       return state.ChatRootReducer.getSenderMessagesReducer.message.chats
-    }else{
+
+    } else {
       return null
     }
   })
   // console.log("all messages",getMessages)
 
 
+  useEffect(() => {
+    dispatch(createChatActions(EnggId, '65d49276f60a227274baf8e1')); //todo - in future the id is dynamic as come from login user
+   
 
-  useEffect(()=>{
-    dispatch(createChatActions(EnggId,'65d49276f60a227274baf8e1')) //todo - in future the id is dynamic as come from login user
-    // console.log("dooon",chatCreated?._id)
-    if (chatCreated?._id) {
-      dispatch(getSenderMessagesAction(chatCreated._id));
-    }
-  },[dispatch,chatCreated?._id])
+    setTimeout(() => {
+      if (chatCreated?._id) {
+        dispatch(getSenderMessagesAction(chatCreated._id));
+      }
+    }, 400)
+    // Cleanup function
+    return () => {
+      if (chatCreated?._id) {
+        dispatch(getSenderMessagesAction()); // Clear sender messages when unmounting
+        dispatch(createChatActions())
+      }
+    };
+  }, [dispatch, chatCreated?._id]);
+
 
 
 
@@ -108,23 +131,39 @@ const MessageBox = ({ onClose,EnggId }) => {
   };
 
   const handleInput = () => {
+
     setHeight(textareaRef.current);
     setSwapIcon(!textareaRef.current.value.trim());
   };
 
 
 
-  const handleSendMessage = () =>{
-    dispatch(sendChatMessageAction('65d49276f60a227274baf8e1',messageData,chatCreated?._id)); //todo - in future the id is dynamic as come from login user
+  const handleSendMessage = () => {
+    dispatch(sendChatMessageAction('65d49276f60a227274baf8e1', messageData, chatCreated?._id)); //todo - in future the id is dynamic as come from login user
     setMessageData('');
-    setTimeout(()=>{
+ 
+
+    if (textareaRef.current) {
+      textareaRef.current.value = '';
+      handleInput();
+
+    }
+
+
+
+    setTimeout(() => {
       if (chatCreated?._id) {
         dispatch(getSenderMessagesAction(chatCreated._id));
       }
-    },400)
 
+    }, 400)
 
   }
+
+  useLayoutEffect(() => {
+    scroll(); 
+  }, [getMessages]);
+
 
   return (
     <div className="message-parent-div">
@@ -133,41 +172,40 @@ const MessageBox = ({ onClose,EnggId }) => {
           <div className="pro-heading"></div>
           <div className="featured-icon">
             <div>
-            <MdAddCall />
+              <MdAddCall />
             </div>
             <div>
               <RxCross2 onClick={onClose} />
+
             </div>
           </div>
         </div>
 
-        <div className="message-body">
+        <div className="message-body" ref={messageBodyRef}>
+          {getMessages?.length > 0 ? (
 
-              {getMessages?.map((item) => {
-                console.log("message item map", item.ChatId);
-                socket.emit("join chat", item.ChatId);
-
-
-                const isSender = item.Sender[0] === '65d49276f60a227274baf8e1'; // Assuming loggedInUserId is the ID of the currently logged-in user (is is static but in TODO => future it is dynamic)
+            getMessages.map((item) => {
+              // console.log("message item map", item.Content);
               return (
-                <div className={isSender ? "sender-side" : "reciver-side"}  key={item._id}>
-                  <div className={isSender ? "sender-message" : "reciver-message"}>
+                <div className="sender-side" key={item._id}>
+                  <div className="sender-message">
                     <p>{item.Content}</p>
                   </div>
                 </div>
               );
-            })}
-              
-              
+            })
+          ) : (
 
-          
-          
-      {/* <div className="reciver-side">
+            <div className="loader">Loading...</div>
+          )}
+
+
+          {/* <div className="reciver-side">
             <div className="reciver-message">
               <p>how are you b </p>
             </div>
-          </div> */}
-          
+          </div>   */}
+
 
           {file.length > 0 && (
             <div className="sender-side">
@@ -190,11 +228,11 @@ const MessageBox = ({ onClose,EnggId }) => {
             placeholder="Enter message"
             ref={textareaRef}
             onInput={handleInput}
-            style={{ resize: "none", height: `${textareaHeight}px` ,fontFamily:"Poppins"}}
+            style={{ resize: "none", height: '10px', fontFamily: "Poppins" }}
             className="text-area-message-whatsapp"
             rows="4"
             cols="50"
-            onChange={(e)=> setMessageData(e.target.value)}
+            onChange={(e) => setMessageData(e.target.value)}
             value={messageData}
           />
         </div>
@@ -216,10 +254,10 @@ const MessageBox = ({ onClose,EnggId }) => {
               <MdOutlineAttachFile />
             </div>
           </div>
-          
-            <p className="send-messsage" onClick={handleSendMessage}>
-            {swapIcon ? (<MdOutlineMic />) : ( <MdSend />) }
-            </p>
+
+          <p className="send-messsage" onClick={handleSendMessage}>
+            {swapIcon ? (<MdOutlineMic />) : (<MdSend />)}
+          </p>
         </div>
       </div>
     </div>
