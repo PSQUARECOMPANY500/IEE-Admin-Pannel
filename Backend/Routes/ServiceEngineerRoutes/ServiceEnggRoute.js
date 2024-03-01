017cf2d6edd95c1c32eea62a06f5598a59f2102b
Preet <psqrco@gmail.com>
2024-03-02 02:24:14 +0530

    added middleware for file upload

--- Backend/Routes/ServiceEngineerRoutes/ServiceEnggRoute.js
+++ Backend/Routes/ServiceEngineerRoutes/ServiceEnggRoute.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const multer  = require('multer')
+const EnggAttendanceServiceRecord  = require('../../Modals/ServiceEngineerModals/Attendance')
 const { verifyEnggToken } = require("../../Middleware/ServiceEnggAuthMiddleware")
 
 const serviceEnggContoller = require("../../Controllers/ServiceEngineerContoller/ServiceEnggController");
@@ -24,20 +25,52 @@ router.get('/getEngScheduleData', verifyEnggToken,  serviceEnggContoller.getEngS
 const timestamp = new Date().getTime();
 const filename = `picture_${timestamp}.jpg`;
 
+// Configure multer for handling file uploads
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
-const storage = multer.diskStorage({
+
+// // Define a route for uploading images
+// app.post('/upload', upload.single('image'), async (req, res) => {
+//   const newImage = new Image({
+//     data: req.file.buffer,
+//     contentType: req.file.mimetype,
+//   });
+//   await newImage.save();
+//   res.send('Image uploaded successfully');
+// });
+
+
+// const storage = multer.diskStorage({
   
-    destination: (req, file, cb) => {
-      cb(null, "public/documents");
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.fieldname + '-' + Date.now())
-      }
-  });
-  const upload = multer({ storage: storage });
+  //   destination: (req, file, cb) => {
+  //     cb(null, "public/documents");
+  //   },
+  //   filename: function (req, file, cb) {
+  //       cb(null, file.fieldname + '-' + Date.now())
+  //     }
+  // });
+  // const upload = multer({ storage: storage });
 
 //engg route on check-in check-out firsthalfbreak SecondHalfBreak LunchBreak
-router.post('/enggCheckIn', upload.single('image'), serviceEnggContoller.EnggCheckIn);
+router.post('/enggCheckIn', upload.single('image'), async (req, res) => {
+  const time = new Date().toLocaleTimeString("en-IN", {
+    timeZone: "Asia/Kolkata",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: '2-digit',
+    hour12: false 
+  });
+  const newImage = new EnggAttendanceServiceRecord({
+    image: req.file.buffer,
+    Check_In: {
+      engPhoto: req.file.filename,
+      time: time,
+    }  
+  });
+  await newImage.save();
+  res.send('Image uploaded successfully');
+});
 
 
 
