37f058086ceda5a7c7749352ef7f2054d985feab
Preet <psqrco@gmail.com>
2024-07-24 19:17:04 +0530

    armmann ki api  multer error

--- Backend/Routes/ClientRoutes/ClientRoutes.js
+++ Backend/Routes/ClientRoutes/ClientRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer")
 const jwt = require('jsonwebtoken');
 
 const {verifyToken} = require('../../Middleware/ClientAuthMiddleware')
