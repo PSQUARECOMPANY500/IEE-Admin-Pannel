917369d5c2703a59363ee047fe1bd02c3ba3fbfa
Rahul Kumar <rahulkumar@psquarecompany.com>
2024-07-31 13:20:26 +0530

    added responsiveness to add enginer form

--- Frontend/ieelifts/src/Components/AdminPannel/Component/EngeeniersSubComponent/AddEngineerForm.js
+++ Frontend/ieelifts/src/Components/AdminPannel/Component/EngeeniersSubComponent/AddEngineerForm.js
@@ -1269,7 +1269,7 @@ const AddEngineerForm = ({ engID, onClose }) => {
             </div>
             {/* ----------------------------------------------------------- Work experience Section starts ---------------------------------------------------------------------------- */}
 
-            <div className="addEnggButtons">
+            <div className="addEnggButtons addEnggButtons-view">
               <button
                 onClick={handleEditSection}
                 style={{ background: editchange ? "#F8AC1D" : "#fff" }}
