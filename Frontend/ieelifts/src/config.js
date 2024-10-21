655162615f30c660621f3c53f4572733fdaaaf09
Paraspsquare <paras.sharma@psquarecompany.com>
2024-10-21 15:40:49 +0530

    paras PS channges

--- Frontend/ieelifts/src/config.js
+++ Frontend/ieelifts/src/config.js
@@ -1,12 +1,12 @@
 const config = {
-    // apiUrl: 'https://ieelifts.in/api',
-    // documentUrl: 'https://ieelifts.in/api/document',
-    // apiUrl: 'http://localhost:8000/api',
-    // documentUrl: 'http://localhost:8000/api/document',
-    apiUrl: 'http://192.168.29.44:8000/api',
-    documentUrl: 'http://192.168.29.44:8000/api/document'
-    // apiUrl: 'https://iee-admin-pannel-backend.onrender.com/api',
-    // documentUrl: 'https://iee-admin-pannel-backend.onrender.com/api/document'
+  // apiUrl: 'https://ieelifts.in/api',
+  // documentUrl: 'https://ieelifts.in/api/document',
+  apiUrl: "http://localhost:8000/api",
+  documentUrl: "http://localhost:8000/api/document",
+  //   apiUrl: "http://192.168.29.44:8000/api",
+  //   documentUrl: "http://192.168.29.44:8000/api/document",
+  // apiUrl: 'https://iee-admin-pannel-backend.onrender.com/api',
+  // documentUrl: 'https://iee-admin-pannel-backend.onrender.com/api/document'
 };
 
 export default config;
